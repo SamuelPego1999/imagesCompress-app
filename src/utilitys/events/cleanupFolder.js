b2222b709a0209728607edba509753caef833891
SamuelPego <samuelpego562@gmail.com>
2024-05-17 02:16:36 -0400

    changing modified controllers, changing config.js

--- src/utilitys/events/cleanupFolder.js
+++ src/utilitys/events/cleanupFolder.js
@@ -1,11 +1,13 @@
 import { EventEmitter } from "events";
 import fs from "fs/promises";
 import { join } from "path";
+import { compressedFilesPath } from "../../config.js";
+
 
 const customEmitter = new EventEmitter();
 
 export const cleanupEvent = customEmitter.on("cleanupFolder", async () => {
-  const folderPath = "src/public/filesCompressed";
+  const folderPath = compressedFilesPath;
   const folderContent = await fs.readdir(folderPath);
   folderContent.forEach(async (file) => {
     console.log(file);
