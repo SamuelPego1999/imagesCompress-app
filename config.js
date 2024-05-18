30a600c640c24ae395cfff411e56727a24e26175
SamuelPego <samuelpego562@gmail.com>
2024-05-17 21:57:11 -0400

    config.js changed

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ import {join,dirname} from "path";
 export const PORT = process.env.PORT || 3000;
 export const compressedFilesPath = join(dirname(fileURLToPath(import.meta.url)),"src","public","filesCompressed");
 
+
